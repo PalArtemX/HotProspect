18243faae1367e1f8163de8eb8812979ff251b38
Artem Paliutin <pal.artem@icloud.com>
2022-05-24 17:37:03 +0300

    start

---
+++ HotProspect/Views/ProspectsView.swift
@@ -0,0 +1,29 @@
+//
+//  ProspectsView.swift
+//  HotProspect
+//
+//  Created by Artem Paliutin on 24/05/2022.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+struct ProspectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProspectsView()
+    }
+}

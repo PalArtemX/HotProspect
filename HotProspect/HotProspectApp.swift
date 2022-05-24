47fd006cf77eb638afee4b80405e2a96688e4129
Artem Paliutin <pal.artem@icloud.com>
2022-05-24 17:11:26 +0300

    Initial Commit

---
+++ HotProspect/HotProspectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectApp.swift
+//  HotProspect
+//
+//  Created by Artem Paliutin on 24/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
